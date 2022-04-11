7acb4692840a3365ef8e1d30c63ee696f84a0ddc
José Lucas R. Santos <joselucasrs@gmail.com>
2022-04-11 09:57:45 -0300

    Creating codelens based on the line of the comment.

---
+++ src/myCodeLensProvider.ts
@@ -0,0 +1,38 @@
+import {
+  CancellationToken,
+  CodeLens,
+  CodeLensProvider,
+  ProviderResult,
+  Range,
+  TextDocument,
+  Command,
+} from 'vscode';
+
+export class MyCodeLensProvider implements CodeLensProvider {
+  async provideCodeLenses(
+    document: TextDocument,
+    token: CancellationToken,
+  ): Promise<CodeLens[]> {
+    // Define where the CodeLens will exist
+
+    let codeLensArr: CodeLens[] = [];
+
+    for (let i = 0; i < document.lineCount; i += 1) {
+      if (document.lineAt(i).text === '// Start-Block') {
+        // Define what command we want to trigger when activating the CodeLens
+        let c: Command = {
+          command: 'testingcodelens.helloWorld',
+          title: 'Hello World',
+        };
+
+        let topOfDocument = new Range(i, 0, i, 0);
+        let codeLens = new CodeLens(topOfDocument, c);
+
+        codeLensArr.push(codeLens);
+        console.log(i + 1);
+      }
+    }
+
+    return codeLensArr;
+  }
+}
